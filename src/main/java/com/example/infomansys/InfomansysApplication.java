7e8435e8c7633021f995cb7c8dc6481757fc97cd
John Vincent Jakosalem <johnvincentjakosalem@gmail.com>
2026-05-03 11:33:16 +0800

    First Commit

---
+++ src/main/java/com/example/infomansys/InfomansysApplication.java
@@ -0,0 +1,13 @@
+package com.example.infomansys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfomansysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfomansysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/infomansys/InfomansysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infomansys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfomansysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
